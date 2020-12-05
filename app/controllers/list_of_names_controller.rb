68b724cfb4f26edd1b4cfac8db26059e218818fd
Masumi Komatsu <msm0046@icloud.com>
2020-12-05 14:45:54 +0900

    showメソッドに出席の状態を切り替え表示

--- app/controllers/list_of_names_controller.rb
+++ app/controllers/list_of_names_controller.rb
@@ -5,5 +5,19 @@ class ListOfNamesController < ApplicationController
 
   def show
     @name = Name.find_by(id: params[:id])
+    # Attendanceテーブルから@name.idと合致するidを検索する
+    # Attendanceテーブルに合致するidがあれば、Attendanceテーブルのnames_idカラムにnameのidを挿入する
+    attendance = Attendance.find_by(names_id: @name.id)
+    if attendance
+      attendance.names_id = @name.id
+      # names_idカラムに値が入ったら、Attendanceテーブルのattendance_dateカラムに今日の日付を挿入する
+      attendance.attendance_date = Date.today
+      attendance.save
+      @message = "#{@name.name}さん、出席しました"
+    else
+      # Attendanceテーブルに合致するidがなければ、nilを返す
+      # "出席済み"の表示を代入する
+      @message = "#{@name.name}さん、出席済みです"
+    end
   end
 end
