77558e147d96dc63ad52ca7e8fc685049e2f7629
Masumi Komatsu <msm0046@icloud.com>
2020-12-26 12:17:46 +0900

    管理者ページで出席済み・未出席の一覧を表示できるように設定

--- app/controllers/attendance_confirmation_controller.rb
+++ app/controllers/attendance_confirmation_controller.rb
@@ -1,5 +1,8 @@
 class AttendanceConfirmationController < ApplicationController
   def index
+    # TODO: 日付指定に対応する
+    # NOTE: いまは「今日の日付の出席・未出席」の振り分けのみが実装済み
+
     # Nameテーブルから全てのレコードを取得する
     name_list = Name.all
     # Name.all は Array と同じように扱える
@@ -11,18 +14,16 @@ class AttendanceConfirmationController < ApplicationController
       # Attendanceテーブルのidとname_list.idが一致するものがあるか検索する
       #
       # Name レコードから、関連付けを用いて Attendance レコードを持ってくる
-      attendance_list = name.attendances
 
       # 今日の日付と一致するものがあるか、探したい
-      attendance_list.each do |attendance|
-        if attendance.attendance_date == Date.today
-          # 出席済みの場合
-          @alreday_attendant.push(name.name) # push で値を入れてあげる
-        else
-          # 未出席の場合
-          @not_attendant_yet.push(name.name)
-        end
+      if alrealreday_attendant?(name.attendances)
+        # 出席済みの場合
+        @alreday_attendant.push(name.name) # push で値を入れてあげる
+      else
+        # 未出席の場合
+        @not_attendant_yet.push(name.name)
       end
+
       # 配列として扱う: attendance_list.each do...
       # Attendance レコードの最新の日付が、今日と一致すれば、「出席済み」
       # 一致しなければ、「未出席」として扱える
@@ -32,9 +33,15 @@ class AttendanceConfirmationController < ApplicationController
       # 値が入っていなければ、未出席の方にグループ分けする
     end
   end
-end
 
+  private
 
-def alreday_attendant?(attendance)
-  
+  def alreday_attendant?(attendance_list)
+    attendance_list.each do |attendance|
+      # 出席済みの場合
+      return true if attendance.attendance_date == Date.today
+    end
+
+    false
+  end
 end
